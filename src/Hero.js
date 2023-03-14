7141d56b47d960d566bde99b32242bdec94d4a7b
Dev0071 <lecturergacheru71@gmail.com>
2023-03-14 17:44:08 +0300

    set up structure for the hero component

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,31 @@ import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  const { closeSubmeu } = useGlobalContext();
+
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payment infrastructure for the internet</h1>
+          <p>
+            Millions of company of all sizes - from start-ups to fortune 500-
+            uses stripe softwares and APIs to accept payment, send out payment
+            and manage their bussiness online{' '}
+          </p>
+          <button className='btn'>start now</button>
+        </article>
+
+        <article className='hero-images'>
+          <img
+            className='phone-img'
+            src={phoneImg}
+            alt='phone image'
+          />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
